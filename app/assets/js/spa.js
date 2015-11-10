0eba06afcae95aa2ae8dc9f0010c973cb43e2d11
afonsopacifer <afonsopolis@gmail.com>
2015-11-09 22:26:30 -0200

    more content added

--- app/assets/js/spa.js
+++ app/assets/js/spa.js
@@ -1,3 +1,8 @@
-(function() {
-  console.log('run');
-})();
+$('.nav-link').click(function(e){
+  e.preventDefault();
+  var href = $(this).attr('href');
+  $('#main').load(href);
+
+  history.pushState(null, null, href);
+
+});
