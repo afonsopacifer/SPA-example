429054845b99947bbc1637c86fcdcc9f9c49c5cb
afonsopacifer <afonsopolis@gmail.com>
2015-11-21 22:06:03 -0200

    app refactor

--- app/assets/js/spa.js
+++ app/assets/js/spa.js
@@ -1,18 +1,54 @@
-$('#main').load('pagina-1.html');
+$(document).ready(function(){
 
+  var navLink = $('#nav .nav-link'), // Links que fazer a requisição
+      content = '#content', //Conteudo que é requisitado
+      main = $('#main'); //Conteiner que recebe o conteudo
 
-$('.nav-link').on('click', function(e){
-  e.preventDefault();
-  var href = $(this).attr('href');
-  $('#main').load(href);
+  //Adicionado propriedades a primeira pagina aberta pelo browser
+  //--------------------------------------------------------------
+  //Definindo o state
+  var local = window.location.pathname;
+  window.history.pushState({page: local}, null, "");
 
-  history.pushState({page: href}, null, href);
+  menuFeedback();
 
-});
+  //Requisitando o conteudo
+  //--------------------------------------------------------------
+  navLink.on('click', function(e){
+    var href = $(this).attr('href');
+    e.preventDefault();
+    //Carregando o especifco requisitado
+    main.load(href+' '+content);
+    //Definindo o state
+    window.history.pushState({page: href}, null, href);
 
-window.addEventListener('popstate', function(event) {
-  if(event.state) {
-    var content = event.state.page;
-  }
-  $('#main').load(content);
+    menuFeedback();
+  });
+
+  //Garante a navegação as páginas que o usuario já navegou
+  //--------------------------------------------------------------
+  window.addEventListener('popstate', function(event) {
+    //Definindo o state
+    if(event.state) {
+      var pageState = event.state.page;
+    }
+    //Carregando o especifco requisitado
+    main.load(pageState+' '+content);
+
+    menuFeedback();
+  });
+
+  //verifica a pagina atual e adiciona a classe .active ao seu link no menu
+  //--------------------------------------------------------------
+  function menuFeedback() {
+    var local = window.location.pathname;
+    navLink.each(function(){
+      if ( "/"+$(this).attr('href') == local ) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+  };
+
 });
